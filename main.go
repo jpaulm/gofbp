0781c9c732f9c1588a1a198a4e9025e62a4fd782
Egon Elbre <egonelbre@gmail.com>
2021-09-05 12:09:09 +0300

    avoid specifying GOMAXPROCS
    Go will automatically select the value based on the
    number of cores available. This can be provided as an
    environment variable when needed.

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"runtime"
-
 	"github.com/jpaulm/gofbp/components/receiver"
 	"github.com/jpaulm/gofbp/components/sender"
 	"github.com/jpaulm/gofbp/core"
 )
 
 func main() {
-	runtime.GOMAXPROCS(4)
-
 	var net *core.Network = core.NewNetwork("test_net")
 
 	proc := net.NewProc("Sender", sender.Execute)
