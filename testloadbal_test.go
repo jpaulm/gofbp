5006c4548d189e8cac0420f786af22adcc8bd832
Paul Morrison <jpaulmorr@gmail.com>
2021-12-11 10:49:42 -0500

    Added some diagrams; fixed package in testloadbal_test.go

--- testloadbal_test.go
+++ testloadbal_test.go
@@ -10,7 +10,7 @@ import (
 func TestLoadBal(t *testing.T) {
 	net := core.NewNetwork("TestLoadBal")
 
-	proc1 := net.NewProc("Sender", &testrtn.Sender{})
+	sender := net.NewProc("Sender", &testrtn.Sender{})
 
 	proc2 := net.NewProc("LoadBalance", &testrtn.LoadBalance{})
 
@@ -18,8 +18,8 @@ func TestLoadBal(t *testing.T) {
 	proc3b := net.NewProc("Receiver1", &testrtn.DelayedReceiver{})
 	proc3c := net.NewProc("Receiver2", &testrtn.DelayedReceiver{})
 
-	net.Initialize("40", proc1, "COUNT")
-	net.Connect(proc1, "OUT", proc2, "IN", 6)
+	net.Initialize("40", sender, "COUNT")
+	net.Connect(sender, "OUT", proc2, "IN", 6)
 	net.Connect(proc2, "OUT[0]", proc3a, "IN", 6)
 	net.Connect(proc2, "OUT[1]", proc3b, "IN", 6)
 	net.Connect(proc2, "OUT[2]", proc3c, "IN", 6)
