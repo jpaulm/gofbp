498eb0b1f194e41c524ee3f91e531611e5a860ef
Tom <tyoung3@desktop.twyoung.com>
2022-01-31 10:35:21 -0500

    clone README and .gitignore

---
+++ ws_respond.go
@@ -0,0 +1,55 @@
+// https://github.com/gorilla/websocket/blob/master/examples/echo/server.go
+
+package websocket
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+	"github.com/jpaulm/gofbp/core"
+)
+
+type WSRespond struct {
+	ipt core.InputConn
+}
+
+func (WSRespond) MustRun() {}
+
+func (wsrespond *WSRespond) Setup(p *core.Process) {
+	wsrespond.ipt = p.OpenInPort("IN")
+}
+
+func (wsrespond *WSRespond) Execute(p *core.Process) {
+	for {
+		pkt := p.Receive(wsrespond.ipt) // should be open bracket
+		if pkt == nil {
+			return
+		}
+		p.Discard(pkt)
+
+		pkt = p.Receive(wsrespond.ipt) // connection
+		if pkt == nil {
+			return
+		}
+		conn, _ := pkt.Contents.(*websocket.Conn)
+
+		p.Discard(pkt)
+
+		for {
+			pkt = p.Receive(wsrespond.ipt)
+			if pkt.PktType == core.CloseBracket {
+				break
+			}
+
+			err := conn.WriteMessage(websocket.TextMessage, []byte(pkt.Contents.(string)))
+			if err != nil {
+				log.Println("write:", err)
+				break
+			} else {
+				p.Discard(pkt)
+			}
+		}
+		p.Discard(pkt)
+	}
+
+}
